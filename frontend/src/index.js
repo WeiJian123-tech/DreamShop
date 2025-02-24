944f2bbb22505f072d50206dae1dae394711678c
WeiJian123-tech <WeiJianZhen1@gmail.com>
2025-02-24 12:12:11 -0500

    Restructured directory structure. Retyped file paths accordingly.

--- frontend/src/index.js
+++
@@ -1,3 +0,0 @@
-function getShopURL() {
-    window.location.href = "./shop.html";
-}
---
+++ frontend/src/index/index.js
@@ -0,0 +1,3 @@
+function getShopURL() {
+    window.location.href = "/DreamShop/frontend/src/shop/shop.html";
+}
